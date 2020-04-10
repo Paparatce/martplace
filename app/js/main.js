bfb0148a3aa5770663667c08780762ac50e7110d
paparatce <kapustakvahagmail.comgit config --global user.name paparatcegit config --global user.email kapustakvahagmail.com>
2020-04-10 08:45:47 +0300

    added slider top

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function () {
 
- 
+    $('.rate-star').rateYo({
+        rating: 4.5,
+        starWidth: "17px",
+        readOnly: true,
+    });
+
+    $('.slider__top-inner').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        nextArrow: document.querySelector('.slider__top-right'),
+        prevArrow: document.querySelector('.slider__top-left')
+    });
+
+
 
 });
