3b2169ad978dae8892151e345fd5ebeed12aa30c
paparatce <kapustakvaha@gmail.com>
2020-04-27 17:44:58 +0300

    added new-items-page

--- app/js/main.js
+++ app/js/main.js
@@ -61,7 +61,14 @@ $(function () {
     });
 
 
-
+    $('.new__items-tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
+            $('.wrapper .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
 
     $('.products__filter-grid-col').on('click', function () {
         $('.product__list-item').addClass('list');
